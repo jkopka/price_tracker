4e286c4f7df19bf7bb8f77a98082f2a7582fd2c1
Jonathan <48311085+jkopka@users.noreply.github.com>
2020-07-01 14:37:04 +0200

    Korrekturen
    - Abhängigkeit tqdm gelöscht
    - start.sh geändert

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="pricetracker",
     packages=["pricetracker"],
     include_package_data=True,
-    install_requires=["flask", "bs4", "tqdm", "requests", "matplotlib"],
+    install_requires=["flask", "bs4", "requests", "matplotlib"],
 )
--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 
 export FLASK_APP=wsgi.py
 export FLASK_DEBUG=1
-# flask run --host='0.0.0.0'
+flask run --host='0.0.0.0'
